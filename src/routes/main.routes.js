4aa3ec90c50142221f46506ff45d91b58c606843
Abduraxim <toxirmalk@gmail.com>
2024-12-01 20:30:30 +0500

    Dasturda yangilanishlar

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,8 +1,11 @@
 import express from "express"
-import {authRouter, feedbackRouter, userRouter} from "./router.export.js"
+import {authRouter, feedbackRouter,customerInteractionRouter,customerNoteRouter, orderRouter, userRouter} from "./router.export.js"
 
 export const mainRouter=express.Router()
 
 mainRouter.use("/auth",authRouter)
 mainRouter.use("/user",userRouter)
+mainRouter.use("/order",orderRouter)
+mainRouter.use("/customerInteraction", customerInteractionRouter);
+mainRouter.use("/customerNote", customerNoteRouter);
 mainRouter.use("/feedback",feedbackRouter)
