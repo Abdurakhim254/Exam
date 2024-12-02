e66b9b567d620606377334ba49b06ea79a454ed0
Abduraxim <toxirmalk@gmail.com>
2024-12-02 14:18:52 +0500

    Exceptions

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ import { application } from "./src/config/index.js";
 
 const port=application.port
 
+
+process.on('uncaughtException', (err) => {
+    logger.error('Uncaught Exception', err)
+    process.exit(1)
+})
+
+process.on("unhandledRejection", (reason, promise) => {
+    logger.error('Unhandled Rejection at', promise, 'reason', reason)
+    process.exit(1)
+})
+
+
 const serverstart=async()=>{
     try {
         app.listen(port,()=>{
